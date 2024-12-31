393bbf900d34a4de06f1bc078d1d51647d753a75
mafzalbro <mafzalbro@gmail.com>
2024-12-31 19:39:28 +0500

    quicknode_rpc applied

--- src/components/hero/RightHero.jsx
+++ src/components/hero/RightHero.jsx
@@ -2,7 +2,7 @@ import Form from "../right-section/Form";
 
 const RightHero = () => {
   return (
-    <div className="right-section relative flex flex-col justify-center items-center h-full w-full sm:mt-40">
+    <div className="right-section relative flex flex-col justify-center items-center h-full w-full sm:mt-40 my-40">
       <div className="absolute !-top-24 !right-0 z-10 pointer-events-none hidden md:block rotate-animation">
         <div>
           <img src="light.png" alt="light" />
